5b6166c1d88743fa433b1ae286dd49fe822866a0
D-Dursty <devondahlia@gmail.com>
2022-11-01 20:35:09 -0700

    easy prompts and if statements updated to index.js

--- index.js
+++ index.js
@@ -4,47 +4,47 @@ const fs = require('fs');
 
 const db = require('./db/connection');
 
-function promptUser(){
+function promptUser() {
     inquirer.prompt(
         {
-            type:"list",
-            name:"todo",
-            message:"What would you like to do?",
-            choices:["View All Departments","Add Department","View All Employees","Update Employee Role","View All Roles","Add Role","Quit"]
+            type: "list",
+            name: "todo",
+            message: "What would you like to do?",
+            choices: ["View All Departments", "Add Department", "View All Employees", "Update Employee Role", "View All Roles", "Add Role", "Quit"]
         }
     ).then(answers => {
-        // do whatever i want with it
-        console.log(answers); // sanity check
-    
-        // if user chose view depts
         if (answers.todo === "View All Departments") {
             // -- query the dept
-            db.query('SELECT * FROM department', function (err, results){
+            db.query('SELECT * FROM department', function (err, results) {
                 // -- -- console.table the results
                 console.table(results);
                 // -- -- prompt the user for another command
                 // -- -- ex promptUser(); 
                 promptUser();
-            });
-        };
-        // if user chose view rols
+            })
+        } else if (answers.todo === "View All Employees") {
+            db.query('SELECT * FROM employee', function (err, results) {
+                console.table(results);
+                promptUser()
+            })
+        } else if (answers.todo === "View All Roles") {
+            db.query('SELECT * FROM role', function (err, results) {
+                console.table(results);
+                promptUser()
+            })
+        }
+        // if user chose view roles
         // -- query the role
         // -- -- console.table the results
         // -- -- prompt the user for another command
         // -- -- ex promptUser();
-        // if user chose view employes 
-        // -- query the emps
-        // -- -- console.table the results
-        // -- -- prompt the user for another command
-        // -- -- ex promptUser();
-        // ... 
-    
+
+
         // if the user didnt choose any other option
         // end the db which stops the app
         db.end();
-    })
-}
-promptUser();
+    }
+    )} promptUser();
 
 
 
