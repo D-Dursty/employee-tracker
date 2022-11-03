031f138b751b7814f4c32a95b5b1e1768077beb7
D-Dursty <devondahlia@gmail.com>
2022-11-02 20:28:24 -0700

    command line is working, need to add three more functionalities before completion

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,4 @@
--- Active: 1666991930510@@127.0.0.1@3306@companyroster_db
+
 
 SELECT * FROM department;
 SELECT * FROM role;
--- index.js
+++ index.js
@@ -4,16 +4,31 @@ const fs = require('fs');
 
 const db = require('./db/connection');
 
+
 function promptUser() {
     inquirer.prompt(
         {
             type: "list",
             name: "todo",
             message: "What would you like to do?",
-            choices: ["View All Departments", "Add Department", "View All Employees", "Update Employee Role", "View All Roles", "Add Role", "Quit"]
+            choices: [
+                {name:"View All Departments", 
+                value:"view_departments"}, 
+                {name:"Add Department",
+                value:"add_department"}, 
+                {name:"View All Employees", 
+                value:"view_employees"}, 
+                {name:"Update Employee Role",
+                value:"update_role"}, 
+                {name:"View All Roles",
+                value:"view_role"}, 
+                {name:"Add Role",
+                value:"add_role"}, 
+                {name:"Quit",
+                value:"quit"}]
         }
     ).then(answers => {
-        if (answers.todo === "View All Departments") {
+        if (answers.todo === "view_departments") {
             // -- query the dept
             db.query('SELECT * FROM department', function (err, results) {
                 // -- -- console.table the results
@@ -22,17 +37,35 @@ function promptUser() {
                 // -- -- ex promptUser(); 
                 promptUser();
             })
-        } else if (answers.todo === "View All Employees") {
+        } else if (answers.todo === 'add_department') {
+            inquirer.prompt(addDeptPrompt).then((answers) => {
+                newDepartment(answers);
+                promptUser();
+            })
+            
+        } else if (answers.todo === "view_employees") {
             db.query('SELECT * FROM employee', function (err, results) {
                 console.table(results);
                 promptUser()
             })
-        } else if (answers.todo === "View All Roles") {
+        } else if (answers.todo === 'update_role') {
+            console.log('i need to update an employees role here');
+            promptUser()
+        } else if (answers.todo === "view_role") {
             db.query('SELECT * FROM role', function (err, results) {
                 console.table(results);
                 promptUser()
             })
+        } else if (answers.todo === 'add_role') {
+            console.log("i am going to insert a new role here");
+            promptUser()
+        } else {
+            db.end();
         }
+         
+        //  else if (answers.todo === "add_department") {
+        //     db.query('SELECT * FROM department', )
+        // }
         // if user chose view roles
         // -- query the role
         // -- -- console.table the results
@@ -42,12 +75,41 @@ function promptUser() {
 
         // if the user didnt choose any other option
         // end the db which stops the app
-        db.end();
+        
     }
-    )} promptUser();
+    )} 
 
+    const addDeptPrompt = [{
+        type:'input',
+        message: 'What is your new department name?',
+        name:'new_name'
+    }]
 
+    function newDepartment(answers) {
+        db.query('INSERT INTO department SET ?', {
+            name: answers.new_name
+        }, function (err, results){
+            console.table(results);
+        })
+    }
 
+    const addNewRole = [
+        {
+            type:'input',
+            message:'What is the title of the new role you would like to enter?',
+            name:"new_role_input"
+        },
+        {
+            type:'input',
+            message:'What is the salary for this role?',
+            name:'new_role_salary'
+        },
+        {   type:'input',
+            message:'What is the corresponding department ID?',
+            name:'new_role_dept'
+        }]
+
+promptUser();
 
 
 
